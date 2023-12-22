ab4ba062579e92a73ff826263902fbec8b7692c4
shein26 <o.s.shein@student.khai.edu>
2023-12-22 04:58:12 +0200

    hangman 2-nd stage

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,2 +1,24 @@
-package hangman;public class Hangman {
+package Hangman;
+
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+
+        // Етап 2: Вгадування слова
+        System.out.println("Guess the word: ");
+        Scanner scanner = new Scanner(System.in);
+        String secretWord = "java";  // Загадане слово (можна змінити)
+
+        // Введення гравцем слова
+        String guessedWord = scanner.nextLine();
+
+        // Перевірка вгаданого слова
+        if (guessedWord.equals(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+    }
 }

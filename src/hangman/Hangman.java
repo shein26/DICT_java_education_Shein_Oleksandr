3b4d8b7e3e4fce25cdfcc5925f54ff82bb7ae28d
shein26 <o.s.shein@student.khai.edu>
2023-12-22 05:00:54 +0200

    hangman 5-th stage

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -7,7 +7,7 @@ public class Hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
 
-        // Етап 4: Підказка для гравця (перші дві літери)
+        // Етап 5: Гравець вгадує літери
         String[] words = {"python", "java", "javascript", "kotlin"};
         Random random = new Random();
         String secretWord = words[random.nextInt(words.length)];  // Випадковий вибір слова
@@ -15,23 +15,52 @@ public class Hangman {
         // Створення підказки
         StringBuilder hint = new StringBuilder(secretWord.length());
         for (int i = 0; i < secretWord.length(); i++) {
-            if (i < 2) {
-                hint.append(secretWord.charAt(i));
+            hint.append("-");
+        }
+
+        // Змінні для кількості залишених спроб та введених букв
+        int remainingAttempts = 8;
+        StringBuilder guessedLetters = new StringBuilder();
+
+        // Гравець вгадує літери
+        while (remainingAttempts > 0) {
+            System.out.println("Word: " + hint);
+            System.out.println("Attempts left: " + remainingAttempts);
+            System.out.print("Input a letter: ");
+            Scanner scanner = new Scanner(System.in);
+            char guessedLetter = scanner.nextLine().charAt(0);
+
+            // Перевірка, чи буква вже вводилася
+            if (guessedLetters.indexOf(String.valueOf(guessedLetter)) != -1) {
+                System.out.println("You've already guessed this letter. Try another one.");
+                continue;
+            }
+
+            guessedLetters.append(guessedLetter);
+
+            // Перевірка, чи буква зустрічається в слові
+            if (secretWord.indexOf(guessedLetter) != -1) {
+                for (int i = 0; i < secretWord.length(); i++) {
+                    if (secretWord.charAt(i) == guessedLetter) {
+                        hint.setCharAt(i, guessedLetter);
+                    }
+                }
+
+                // Перевірка, чи слово повністю вгадане
+                if (hint.toString().equals(secretWord)) {
+                    System.out.println("Word: " + hint);
+                    System.out.println("You survived!");
+                    break;
+                }
             } else {
-                hint.append("-");
+                System.out.println("That letter doesn't appear in the word");
+                remainingAttempts--;
             }
         }
 
-        // Гравець вгадує слово з підказкою
-        System.out.println("Guess the word " + hint + ": ");
-        Scanner scanner = new Scanner(System.in);
-        String guessedWord = scanner.nextLine();
-
-        // Перевірка вгаданого слова
-        if (guessedWord.equals(secretWord)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
+        if (remainingAttempts == 0) {
+            System.out.println("Thanks for playing!");
+            System.out.println("We'll see how well you did in the next stage");
         }
     }
 }

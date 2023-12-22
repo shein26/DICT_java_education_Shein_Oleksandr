883c897ab82d6f307d7032984b2685c8571ae3fa
shein26 <o.s.shein@student.khai.edu>
2023-12-22 04:59:45 +0200

    hangman 4-nd stage

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -7,13 +7,23 @@ public class Hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
 
-        // Етап 3: Випадковий вибір слова зі списку
+        // Етап 4: Підказка для гравця (перші дві літери)
         String[] words = {"python", "java", "javascript", "kotlin"};
         Random random = new Random();
         String secretWord = words[random.nextInt(words.length)];  // Випадковий вибір слова
 
-        // Гравець вгадує слово
-        System.out.println("Guess the word: ");
+        // Створення підказки
+        StringBuilder hint = new StringBuilder(secretWord.length());
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (i < 2) {
+                hint.append(secretWord.charAt(i));
+            } else {
+                hint.append("-");
+            }
+        }
+
+        // Гравець вгадує слово з підказкою
+        System.out.println("Guess the word " + hint + ": ");
         Scanner scanner = new Scanner(System.in);
         String guessedWord = scanner.nextLine();
 

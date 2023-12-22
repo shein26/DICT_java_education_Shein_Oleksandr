74b482bc1fa451bfbae5b476462a5368bd222142
shein26 <o.s.shein@student.khai.edu>
2023-12-22 05:02:52 +0200

    hangman 6-th stage

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -7,7 +7,7 @@ public class Hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
 
-        // Етап 5: Гравець вгадує літери
+        // Етап 6: Кількість спроб обмежується кількістю помилок
         String[] words = {"python", "java", "javascript", "kotlin"};
         Random random = new Random();
         String secretWord = words[random.nextInt(words.length)];  // Випадковий вибір слова
@@ -40,17 +40,25 @@ public class Hangman {
 
             // Перевірка, чи буква зустрічається в слові
             if (secretWord.indexOf(guessedLetter) != -1) {
+                boolean improvement = false;
                 for (int i = 0; i < secretWord.length(); i++) {
-                    if (secretWord.charAt(i) == guessedLetter) {
+                    if (secretWord.charAt(i) == guessedLetter && hint.charAt(i) == '-') {
                         hint.setCharAt(i, guessedLetter);
+                        improvement = true;
                     }
                 }
 
                 // Перевірка, чи слово повністю вгадане
                 if (hint.toString().equals(secretWord)) {
                     System.out.println("Word: " + hint);
+                    System.out.println("You guessed the word!");
                     System.out.println("You survived!");
                     break;
+                }
+
+                // Вивід повідомлення про поліпшення, якщо буква відкрита
+                if (improvement) {
+                    System.out.println("No improvements");
                 }
             } else {
                 System.out.println("That letter doesn't appear in the word");
@@ -59,8 +67,7 @@ public class Hangman {
         }
 
         if (remainingAttempts == 0) {
-            System.out.println("Thanks for playing!");
-            System.out.println("We'll see how well you did in the next stage");
+            System.out.println("You lost!");
         }
     }
 }

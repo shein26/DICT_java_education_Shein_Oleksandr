274561359174631ea6b011729d2ef9b440e12b95
shein26 <o.s.shein@student.khai.edu>
2023-12-22 05:09:37 +0200

    hangman 8-th stage

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,82 +1,104 @@
-package Hangman;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
+
+    private static final String[] WORDS = {"python", "java", "javascript", "kotlin"};
+    private static final int MAX_TRIES = 8;
+
     public static void main(String[] args) {
-        System.out.println("HANGMAN");
+        Scanner mainScanner = new Scanner(System.in);
 
-        // Етап 7: Перевірка на різні типи помилок та виправлення кількості спроб
-        String[] words = {"python", "java", "javascript", "kotlin"};
-        Random random = new Random();
-        String secretWord = words[random.nextInt(words.length)];
+        while (true) {
+            System.out.println("HANGMAN");
+            System.out.print("Type \"play\" to play the game, \"exit\" to quit: ");
 
-        StringBuilder hint = new StringBuilder(secretWord.length());
-        for (int i = 0; i < secretWord.length(); i++) {
-            hint.append("-");
-        }
+            String choice = mainScanner.nextLine();
 
-        int remainingAttempts = 8;
-        StringBuilder guessedLetters = new StringBuilder();
-
-        while (remainingAttempts > 0) {
-            System.out.println("Word: " + hint);
-            System.out.println("Attempts left: " + remainingAttempts);
-            System.out.print("Input a letter: ");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-
-            // Перевірка, чи введена одна буква
-            if (input.length() != 1) {
-                System.out.println("You should input a single letter");
-                continue;
-            }
-
-            char guessedLetter = input.charAt(0);
-
-            // Перевірка, чи введено малу англійську букву
-            if (!Character.isLowerCase(guessedLetter)) {
-                System.out.println("Please enter a lowercase English letter");
-                continue;
-            }
-
-            // Перевірка, чи буква вже була введена
-            if (guessedLetters.indexOf(String.valueOf(guessedLetter)) != -1) {
-                System.out.println("You've already guessed this letter");
-                continue;
-            }
-
-            guessedLetters.append(guessedLetter);
-
-            // Перевірка, чи буква зустрічається в слові
-            if (secretWord.indexOf(guessedLetter) != -1) {
-                boolean improvement = false;
-                for (int i = 0; i < secretWord.length(); i++) {
-                    if (secretWord.charAt(i) == guessedLetter && hint.charAt(i) == '-') {
-                        hint.setCharAt(i, guessedLetter);
-                        improvement = true;
-                    }
-                }
-
-                if (hint.toString().equals(secretWord)) {
-                    System.out.println("Word: " + hint);
-                    System.out.println("You guessed the word!");
-                    System.out.println("You survived!");
-                    break;
-                }
-
-                if (improvement) {
-                    System.out.println("No improvements");
-                }
+            if (choice.equals("exit")) {
+                System.out.println("Goodbye, have a nice day!");
+                break;
+            } else if (choice.equals("play")) {
+                playHangmanGame();
             } else {
-                System.out.println("That letter doesn't appear in the word");
-                remainingAttempts--;
+                System.out.println("Invalid choice. Please enter \"play\" or \"exit\".");
             }
         }
 
-        if (remainingAttempts == 0) {
-            System.out.println("You lost!");
+        mainScanner.close();
+    }
+
+    private static void playHangmanGame() {
+        Random random = new Random();
+        String wordToGuess = WORDS[random.nextInt(WORDS.length)];
+        char[] guessedWord = new char[wordToGuess.length()];
+        boolean[] lettersGuessed = new boolean[26];
+        int triesLeft = MAX_TRIES;
+
+        for (int i = 0; i < guessedWord.length; i++) {
+            guessedWord[i] = '-';
         }
+
+        while (triesLeft > 0) {
+            System.out.println("Word: " + new String(guessedWord));
+            System.out.println("Tries left: " + triesLeft);
+            System.out.print("Input a letter: ");
+
+            Scanner scanner = new Scanner(System.in);
+
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.equals("exit")) {
+                System.out.println("Exiting the game...");
+                return;
+            }
+
+            if (input.length() != 1 || !Character.isLetter(input.charAt(0))) {
+                System.out.println("Please enter a single letter.");
+                continue;
+            }
+
+            char guess = input.charAt(0);
+
+            if (lettersGuessed[guess - 'a']) {
+                System.out.println("You've already guessed this letter.");
+                continue;
+            }
+
+            lettersGuessed[guess - 'a'] = true;
+
+            boolean letterFound = false;
+
+            for (int i = 0; i < wordToGuess.length(); i++) {
+                if (wordToGuess.charAt(i) == guess) {
+                    guessedWord[i] = guess;
+                    letterFound = true;
+                }
+            }
+
+            if (!letterFound) {
+                System.out.println("That letter doesn't appear in the word");
+                triesLeft--;
+            }
+
+            if (wordGuessed(guessedWord)) {
+                System.out.println("You guessed the word!");
+                System.out.println("You survived!");
+                break;
+            }
+        }
+
+        if (triesLeft == 0) {
+            System.out.println("You lost! The correct word was: " + wordToGuess);
+        }
+    }
+
+    private static boolean wordGuessed(char[] guessedWord) {
+        for (char c : guessedWord) {
+            if (c == '-') {
+                return false;
+            }
+        }
+        return true;
     }
 }
